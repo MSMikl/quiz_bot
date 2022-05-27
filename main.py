a1529dbad63cc6f4c188661f86c7ead28c7ae3b5
MSMikl <michalbl4@gmail.com>
2022-05-27 11:08:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import json
 import os
+import random
 
-from telegram import Update
-from telegram.ext import Updater, CallbackContext, MessageHandler, Filters
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import (
+    Updater, CallbackContext, MessageHandler, Filters, CommandHandler
+)
 
 from dotenv import load_dotenv
 
@@ -13,17 +17,47 @@ def echo(update: Update, context: CallbackContext):
     )
 
 
+def start(update: Update, context: CallbackContext):
+    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
+    keyboard = ReplyKeyboardMarkup(custom_keyboard)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Привет! Я бот для викторин',
+        reply_markup=keyboard
+    )
+
+
+def quiz(update: Update, context: CallbackContext):
+    if update.message.text == 'Новый вопрос':
+        question = random.choice(
+            list(context.bot_data.get('questions', {}).values())
+        )
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=question.get('вопрос')
+        )
+
+
 def main():
     load_dotenv()
     tg_token = os.getenv('TG_TOKEN')
     updater = Updater(token=tg_token)
     dispatcher = updater.dispatcher
-    echo_hanlder = MessageHandler(
-        Filters.text,
-        echo
-    )
+    with open(
+        os.path.join(os.path.curdir, 'questions', 'questions.json'),
+        'r'
+    ) as file:
+        questions = json.load(file)
+    dispatcher.bot_data['questions'] = questions
 
-    dispatcher.add_handler(echo_hanlder)
+    text_handler = MessageHandler(
+        Filters.text & (~Filters.command),
+        quiz
+    )
+    start_handler = CommandHandler('start', start)
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(text_handler)
     updater.start_polling()
 
 
