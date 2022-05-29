a7884f6506271012571170c27d3c9dbffc150532
MSMikl <michalbl4@gmail.com>
2022-05-29 09:04:27 +0300

    ConversationHandler added

--- main.py
+++ main.py
@@ -6,17 +6,14 @@ import redis
 
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import (
-    Updater, CallbackContext, MessageHandler, Filters, CommandHandler
+    Updater, CallbackContext, MessageHandler,
+    Filters, CommandHandler, ConversationHandler
 )
 
 from dotenv import load_dotenv
 
 
-def echo(update: Update, context: CallbackContext):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=update.message.text
-    )
+NEW_QUESTION = 0
 
 
 def start(update: Update, context: CallbackContext):
@@ -27,32 +24,44 @@ def start(update: Update, context: CallbackContext):
         text='Привет! Я бот для викторин',
         reply_markup=keyboard
     )
+    return NEW_QUESTION
 
 
-def quiz(update: Update, context: CallbackContext):
-    if update.message.text == 'Новый вопрос':
-        question = random.choice(
-            list(context.bot_data.get('questions', {}).values())
-        )
+def handle_new_question_request(update: Update, context: CallbackContext):
+    print('question')
+    question = random.choice(
+        list(context.bot_data.get('questions', {}).values())
+    )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=question.get('вопрос')
+    )
+    context.chat_data['answer'] = question.get('ответ').split('.')[0]
+    print('Вопрос:\n{}'.format(question.get('вопрос')))
+    print('Ответ:\n{}'.format(question.get('ответ')))
+    return NEW_QUESTION
+
+
+def handle_solution_attempt(update: Update, context: CallbackContext):
+    print('answer')
+    answer = context.chat_data.get('answer')
+    if answer:
+        if update.message.text.lower() == answer.lower():
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text='Верно!'
+            )
+        else:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text='Неправильный ответ'
+            )
+    else:
         context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text=question.get('вопрос')
+            text='Прежде чем отвечать, получите вопрос'
         )
-        context.chat_data['answer'] = question.get('ответ').split('.')[0]
-        print(context.chat_data['answer'])
-    else:
-        if context.chat_data.get('answer'):
-            if update.message.text.lower() == context.chat_data.get('answer').lower():
-                context.bot.send_message(
-                    chat_id=update.effective_chat.id,
-                    text='Верно!'
-                )
-            else:
-                context.bot.send_message(
-                    chat_id=update.effective_chat.id,
-                    text='Неправильный ответ'
-                )
-
+    return NEW_QUESTION
 
 
 def main():
@@ -74,15 +83,28 @@ def main():
         db=0
     )
     dispatcher.bot_data['db'] = db
+    print('Бот запущен')
 
-    text_handler = MessageHandler(
-        Filters.text & (~Filters.command),
-        quiz
-    )
     start_handler = CommandHandler('start', start)
 
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(text_handler)
+    conversation = ConversationHandler(
+        entry_points=[(start_handler)],
+        states={
+            NEW_QUESTION: [
+                MessageHandler(
+                    (Filters.text(['Новый вопрос']) & (~Filters.command)),
+                    handle_new_question_request
+                ),
+                MessageHandler(
+                    (Filters.text & (~Filters.text(['Новый вопрос'])) & (~Filters.command)),
+                    handle_solution_attempt
+                )
+            ]
+        },
+        fallbacks=[]
+    )
+
+    dispatcher.add_handler(conversation)
     updater.start_polling()
 
 
